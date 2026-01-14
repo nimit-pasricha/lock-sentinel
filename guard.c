31d712c35c848d51122c55868adabf0a3e252ba7
nimit-pasricha <pasricha.nimit@gmail.com>
2026-01-14 16:49:28 +0400

    Actually find orig lock/unlock functions outside the wrappers.

--- guard.c
+++ guard.c
@@ -10,3 +10,11 @@ typedef int (*pthread_mutex_unlock_t) (pthread_mutex_t *);
 
 static pthread_mutex_lock_t real_lock_fn = NULL;
 static pthread_mutex_unlock_t real_unlock_fn = NULL;
+
+__attribute__((constructor))
+void init_guard() {
+    // dlsym finds address of requested function
+    // RTLD_NEXT to skip the one in this file and find the next one in library order. 
+    real_lock_fn = (pthread_mutex_lock_t) dlsym(RTLD_NEXT, "pthread_mutex_lock");
+    real_unlock_fn = (pthread_mutex_lock_t) dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+}
