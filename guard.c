9d7aaad08517d1fc64e779cf67d59212c7f7791f
nimit-pasricha <pasricha.nimit@gmail.com>
2026-01-14 16:53:57 +0400

    Formatting.

--- guard.c
+++ guard.c
@@ -1,26 +1,31 @@
-#define _GNU_SOURCE   // for RTLD_NEXT
+#define _GNU_SOURCE // for RTLD_NEXT
 
 #include <stdio.h>
-#include <dlfcn.h>    // For dlsym() to find the real functions
-#include <pthread.h> 
-#include <unistd.h>  
+#include <dlfcn.h> // For dlsym() to find the real functions
+#include <pthread.h>
+#include <unistd.h>
 
-typedef int (*pthread_mutex_lock_t) (pthread_mutex_t *);
-typedef int (*pthread_mutex_unlock_t) (pthread_mutex_t *);
+typedef int (*pthread_mutex_lock_t)(pthread_mutex_t *);
+typedef int (*pthread_mutex_unlock_t)(pthread_mutex_t *);
 
 static pthread_mutex_lock_t real_lock_fn = NULL;
 static pthread_mutex_unlock_t real_unlock_fn = NULL;
 
-__attribute__((constructor))
-void init_guard() {
+__attribute__((constructor)) void init_guard()
+{
     // dlsym finds address of requested function
-    // RTLD_NEXT to skip the one in this file and find the next one in library order. 
-    real_lock_fn = (pthread_mutex_lock_t) dlsym(RTLD_NEXT, "pthread_mutex_lock");
-    real_unlock_fn = (pthread_mutex_lock_t) dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+    // RTLD_NEXT to skip the one in this file and find the next one in library order.
+    real_lock_fn = (pthread_mutex_lock_t)dlsym(RTLD_NEXT, "pthread_mutex_lock");
+    real_unlock_fn = (pthread_mutex_lock_t)dlsym(RTLD_NEXT, "pthread_mutex_unlock");
 
-    if (!real_lock_fn || !real_unlock_fn) {
+    if (!real_lock_fn || !real_unlock_fn)
+    {
         fprintf(stderr, "[FATAL] init_guard: Failed to find real pthread functions.\n");
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "[INFO] init_guard: Library loaded successfully.\n");
     }
 }
+
+// TODO: write wrappers
