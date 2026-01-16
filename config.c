6d7360435412a28633efd15b4d809b46ece90378
nimit-pasricha <pasricha.nimit@gmail.com>
2026-01-16 12:37:37 +0400

    Handle missing config file.

--- config.c
+++ config.c
@@ -1,5 +1,13 @@
 #include "config.h"
 
+#include <stdio.h>
+
 sentinel_config_t global_config = {.policy = FREEZE};
 
-
+void load_config() {
+    FILE *fp = fopen("sentinel.conf", "r");
+    if (!fp) {
+        fprintf(stderr, "[WARNING] load_config: no config file found. Using default: FREEZE");
+        return;
+    }
+}
