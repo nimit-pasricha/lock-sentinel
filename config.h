d8b1d6f2e367d1534e7e247ef866651efed02595
nimit-pasricha <pasricha.nimit@gmail.com>
2026-01-16 12:31:21 +0400

    Define currently selected policy and config loader.

--- config.h
+++ config.h
@@ -2,10 +2,20 @@
 #define CONFIG_H
 
 typedef enum {
-    RETURN,    // Don't lock. Return EDEADLK
-    FREEZE,    // Report the deadlock, and let it happen
-    STALL      // If deadlock about to occur, put called thread to sleep until safe
-    // TODO: priority
+    RETURN, // Don't lock. Return EDEADLK
+    FREEZE, // Report the deadlock, and let it happen
+    STALL   // If deadlock about to occur, put called thread to sleep until safe
+    // TODO: priority based reordering
 } sentinel_policy_t;
+
+typedef struct {
+    sentinel_policy_t policy;
+    // TODO: add log file paths
+} sentinel_config_t;
+
+extern sentinel_config_t global_config;
+
+// load settings from sentinel.conf
+void load_config();
 
 #endif
